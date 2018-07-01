582ac561436eb2903f4997732b872dcf0665f709
Chaitanya <swift3.0devlopment@gmail.com>
2018-07-02 00:51:37 +0530

    First Commit

---
+++ Tripoye/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tripoye
+//
+//  Created by Chaitanya Patil on 01/07/18.
+//  Copyright © 2018 Chaitanya Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
